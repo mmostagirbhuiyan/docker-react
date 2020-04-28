16950c18045603b3b539e1c6b0707ca959936525
mmostagirbhuiyan <mmostagirbhuiyan@gmail.com>
2020-04-28 06:12:05 -0400

    changed feature

--- src/App.js
+++ src/App.js
@@ -8,7 +8,7 @@ function App() {
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-          Rifa Rafia and M Mostagir Bhuiyan
+          I love my wife Rifa. Your's truly - M Bhuiyan! 
         </p>
         <a
           className="App-link"
